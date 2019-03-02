50a5de78c195f0a12c1ec9a891d880ab83c22c02
tsimnujhawj <tsimnuj.hawj@gmail.com>
2019-03-02 15:41:47 -0600

    added player movement with deltaTime

---
+++ Space Forge/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] float moveXSpeed = 25f;
+    [SerializeField] float moveYSpeed = 25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveXSpeed;
+        var newXPos = transform.position.x + deltaX;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveYSpeed;
+        var newYPos = transform.position.y + deltaY;
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}

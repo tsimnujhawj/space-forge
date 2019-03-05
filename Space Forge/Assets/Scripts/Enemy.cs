e9d9760d700f286020021cf2c7469616486d17ab
tsimnujhawj <tsimnuj.hawj@gmail.com>
2019-03-04 18:33:41 -0600

    enemy lasers

--- Space Forge/Assets/Scripts/Enemy.cs
+++ Space Forge/Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,17 +7,41 @@ public class Enemy : MonoBehaviour
 {
 
     [SerializeField] float health = 100;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = .2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject enemyLaserPrefab;
+    [SerializeField] float enemyProjectileSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0f)
+        {
+            Fire();
+            shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject enemyLaser = Instantiate(
+            enemyLaserPrefab,
+            transform.position,
+            Quaternion.identity) as GameObject;
+        enemyLaser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -enemyProjectileSpeed);
     }
 
     private void OnTriggerEnter2D(Collider2D other)

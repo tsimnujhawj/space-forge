de1c594d557a5574909d45caaeeeb55a4b1590ac
tsimnujhawj <tsimnuj.hawj@gmail.com>
2019-03-07 11:44:12 -0600

    added gamesession

---
+++ Space Forge/Assets/Scripts/GameSession.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSession : MonoBehaviour
+{
+
+    int score = 0;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        SetSingleton();
+    }
+
+    private void SetSingleton()
+    {
+        int numberOfGameSessions = FindObjectsOfType<GameSession>().Length;
+        if (numberOfGameSessions > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void AddToScore(int scoreValue)
+    {
+        score += scoreValue;
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+
+}
